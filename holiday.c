68eef5e69217aceac8ea291f1f70dffa4e828631
karthikkamesh <36397547+karthikkamesh@users.noreply.github.com>
2018-03-08 17:43:05 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char str[50];
+ printf("\n enter the day:");
+ scanf("%s",str);
+ if(str=="sunday"&&str=="saturday")
+ printf("YES");
+ else
+ printf("NO");
+ getch();
+ }
