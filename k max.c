cbbb2a91a32bb8f77f41796f06af43057293285c
karthikkamesh <36397547+karthikkamesh@users.noreply.github.com>
2018-03-09 18:00:34 +0530

    Create k max.c

---
+++ k max.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a[5],p,n;
+ printf("enter the position of the integer:");
+ scanf("%d",p);
+ printf("enter the no of times repeat:");
+ scanf("%d",n);
+ printf("enter the values:");
+ for(int i=1;i<=5;i++)
+ {
+ scanf("%d",a[i]);
+ }
+ if(a[p]!=0)
+ {
+  a[p]=a;
+  while(a[p]<=n)
+  {
+   printf(a[p];
+   n++;
+   }
+ }
+ return 0;
+ }
+   
