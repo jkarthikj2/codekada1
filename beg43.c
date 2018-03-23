c0c301a389ff6f59a64351ed84b779fc4d324384
J.KARTHIK <36397547+karthikkamesh@users.noreply.github.com>
2018-03-23 17:38:33 +0530

    Create beg43.c

---
+++ beg43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ char s1[100],s2[100];
+ printf("\n enter two strings:");
+ scanf("%s%s",s1,s2);
+ if(s1!=s2)
+ printf("\n the result is:%s%s",s1,s2);
+ }
+ 
