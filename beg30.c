db718a462f74cfa995af7a05d457c75abb7490a9
J.KARTHIK <36397547+karthikkamesh@users.noreply.github.com>
2018-05-22 11:42:57 +0530

    Update beg30.c

--- beg30.c
+++ beg30.c
@@ -3,10 +3,10 @@
 void main()
 {
 int t,mn,t1,mn1,t2,mn2;
-printf("\n enter the time and minutes:");
+printf("\n enter the 1st hour and minutes:");
 scanf("%d",&t1);
 scanf("%d",&mn1);
-printf("\n enter the 2nd time and minutes:");
+printf("\n enter the 2nd hour and minutes:");
 scanf("%d",&t2);
 scanf("%d",&mn2);
 if((t1!=t2)&&(mn1!=mn2))
