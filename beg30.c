9c66a58d0347882e1c09a7eb61d0c4c62da1cbf3
J.KARTHIK <36397547+karthikkamesh@users.noreply.github.com>
2018-05-22 11:38:56 +0530

    Create beg30.c

---
+++ beg30.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t,mn,t1,mn1,t2,mn2;
+printf("\n enter the time and minutes:");
+scanf("%d",&t1);
+scanf("%d",&mn1);
+printf("\n enter the 2nd time and minutes:");
+scanf("%d",&t2);
+scanf("%d",&mn2);
+if((t1!=t2)&&(mn1!=mn2))
+{
+ if(t1>t2)
+ t=t1-t2;
+ else
+ t=t2-t1;
+ if(mn1>mn2)
+ mn=mn1-mn2;
+ else
+ mn=mn2-mn1;
+ printf("\n difference of time is:%d H:%d M",t,mn);
+ }
+ else
+ printf("\n !!!The two times or minutes are same!!!");
+ };
