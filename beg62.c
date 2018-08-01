1b9087f75d0a996b6a3cf815d38d67f82cd9bc6e
J.KARTHIK <36397547+karthikkamesh@users.noreply.github.com>
2018-08-01 09:25:26 -0700

    Update beg62.c

--- beg62.c
+++ beg62.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
-{int c=0;
-    char a[10];
-    scanf("%s",&a[5]);
-    for(int i=0;a[i]<5;i++)
-    {
-        if(a[i]>=0&&a[i]<2)
-        c++;
-        else
-        c=0;
-    }
-    if(c==0)
-    printf("no");
-    else
-    printf("yes");
-    
+{
+int i,c,t=0;
+char s[100];
+gets(s);
+c=strlen(s);
+for(i=0;i<c;i++)
+{
+ if(s[i]!=48&&s[i]!=49)
+ {t=1;break;}
+ else
+ continue;
+}
+if(t==1)
+printf("NO");
+else
+printf("YES");
 }
